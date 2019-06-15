26dbf526a911349e572bd184130caea9c9a67873
P Ramya <ramyabhatpersonalid@gmail.com>
2019-06-15 12:38:05 +0530

    Initial commit

---
+++ RecipeBook/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// Data binding in action
+export class AppComponent {
+  name = 'Ramya bhat';
+}
---
+++ RecipeBook/src/app/app.module.ts
@@ -0,0 +1,18 @@
+//For understanding of typescript.These are typescript features
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+// For the understanding of the Angular
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
